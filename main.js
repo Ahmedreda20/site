3d82929029df5649cb4e36463308c343a7aa4992
Ahmedreda20 <62707388+Ahmedreda20@users.noreply.github.com>
2020-05-10 12:58:02 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -87,13 +87,21 @@ function getCookie(c_name) {
 
          ul.appendChild(elem);
          elem.style.textAlign = "center";
+         elem.style.borderBottom = "none";
 
 
         if(getCookie('praise') === undefined){
-            elem.innerHTML = "عدد التسابيح في المرة السابقة كان 0 " ;
+            elem.innerHTML = "  عدد التسابيح في المرة السابقة كان 0 مره" ;
         }else{
-            elem.innerHTML = "عدد التسابيح في المرة السابقة كان  " + getCookie('praise');
+            elem.innerHTML = "عدد التسابيح في المرة السابقة كان  " + getCookie('praise') + " مره";
         }
+        if(getCookie('praise') <= 10 ){
+            elem.innerHTML = "عدد التسابيح في المرة السابقة كان  " + getCookie('praise') + " مرات";
+        }else{
+            elem.innerHTML = "عدد التسابيح في المرة السابقة كان  " + getCookie('praise') + " مره";
+
+        }
+
         
      });  
             var bars = document.querySelector('.ContainerBox');
@@ -112,56 +120,3 @@ function getCookie(c_name) {
          /* TypeWriter*/
 
 
-   
-         var TxtType = function(el, toRotate, period) {
-            this.toRotate = toRotate;
-            this.el = el;
-            this.loopNum = 0;
-            this.period = parseInt(period, 10) || 2000;
-            this.txt = '';
-            this.tick();
-            this.isDeleting = false;
-        };
-    
-        TxtType.prototype.tick = function() {
-            var i = this.loopNum % this.toRotate.length;
-            var fullTxt = this.toRotate[i];
-    
-            if (this.isDeleting) {
-            this.txt = fullTxt.substring(0, this.txt.length - 1);
-            } else {
-            this.txt = fullTxt.substring(0, this.txt.length + 1);
-            }
-    
-            this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-    
-            var that = this;
-            var delta = 200 - Math.random() * 100;
-    
-            if (this.isDeleting) { delta /= 2; }
-    
-            if (!this.isDeleting && this.txt === fullTxt) {
-            delta = this.period;
-            this.isDeleting = true;
-            } else if (this.isDeleting && this.txt === '') {
-            this.isDeleting = false;
-            this.loopNum++;
-            delta = 500;
-            }
-    
-            setTimeout(function() {
-            that.tick();
-            }, delta);
-        };
-    
-        window.onload = function() {
-            var elements = document.getElementsByClassName('typewrite');
-            for (var i=0; i<elements.length; i++) {
-                var toRotate = elements[i].getAttribute('data-type');
-                var period = elements[i].getAttribute('data-period');
-                if (toRotate) {
-                  new TxtType(elements[i], JSON.parse(toRotate), period);
-                }
-            }
-
-        };
