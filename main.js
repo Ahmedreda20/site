128a988deded26981ac1d0e742aa4bf7ae0cc1e7
Ahmedreda20 <62707388+Ahmedreda20@users.noreply.github.com>
2020-05-10 12:30:20 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,167 @@
+function showTime(){
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var session = "AM";
+
+
+    if(h == 0){
+        h = 12;
+    }
+    
+    if(h > 12){
+        h = h - 12;
+        session = "PM";
+    }
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("MyClockDisplay").innerText = time;
+    document.getElementById("MyClockDisplay").textContent = time;
+    
+    setTimeout(showTime, 1000);
+
+}
+
+showTime();
+
+    
+
+
+    function setCookie(c_name, value, exdays) {
+        var exdate = new Date();
+        exdate.setDate(exdate.getDate() + exdays);
+        var c_value = escape(value) + ((exdays == null) ? "" : "; expires=" + exdate.toUTCString());
+        document.cookie = c_name + "=" + c_value;
+}
+function getCookie(c_name) {
+    var i, x, y, ARRcookies = document.cookie.split(";");
+    for (i = 0; i < ARRcookies.length; i++) {
+        x = ARRcookies[i].substr(0, ARRcookies[i].indexOf("="));
+        y = ARRcookies[i].substr(ARRcookies[i].indexOf("=") + 1);
+        x = x.replace(/^\s+|\s+$/g, "");
+        if (x == c_name) {
+            return unescape(y);
+        }
+    }
+    }
+    let count = 0;
+        var div = document.getElementById("btn");
+        var box = document.querySelector('.preview');
+    function myFunction() {
+
+        var p = document.createElement('P');
+        p.classList.add('value');
+
+        p.innerHTML = "استغفر الله العظيم";
+        count+=1;
+        setInterval( function(){
+            p.style.display  = "none";
+        }, 600);
+
+
+        div.innerHTML = count;
+
+        box.appendChild(p);
+
+
+
+     setCookie('praise' , div.innerHTML);
+
+    }
+
+        div.addEventListener('keypress' , function(){
+            div.innerHTML = count;
+        count+=1;
+
+        });
+     window.addEventListener('load' , function(){
+
+         var elem = document.createElement('LI');
+
+         var ul = document.querySelector('.header');
+
+         ul.appendChild(elem);
+         elem.style.textAlign = "center";
+
+
+        if(getCookie('praise') === undefined){
+            elem.innerHTML = "عدد التسابيح في المرة السابقة كان 0 " ;
+        }else{
+            elem.innerHTML = "عدد التسابيح في المرة السابقة كان  " + getCookie('praise');
+        }
+        
+     });  
+            var bars = document.querySelector('.ContainerBox');
+
+     function OpenBar(){
+         bars.style.left = "0";
+     }    
+         var closeEl = document.querySelector('.btn-close');
+     closeEl.addEventListener('click' , function(){
+            bars.style.left = "-300px";
+
+         });
+
+
+
+         /* TypeWriter*/
+
+
+   
+         var TxtType = function(el, toRotate, period) {
+            this.toRotate = toRotate;
+            this.el = el;
+            this.loopNum = 0;
+            this.period = parseInt(period, 10) || 2000;
+            this.txt = '';
+            this.tick();
+            this.isDeleting = false;
+        };
+    
+        TxtType.prototype.tick = function() {
+            var i = this.loopNum % this.toRotate.length;
+            var fullTxt = this.toRotate[i];
+    
+            if (this.isDeleting) {
+            this.txt = fullTxt.substring(0, this.txt.length - 1);
+            } else {
+            this.txt = fullTxt.substring(0, this.txt.length + 1);
+            }
+    
+            this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+    
+            var that = this;
+            var delta = 200 - Math.random() * 100;
+    
+            if (this.isDeleting) { delta /= 2; }
+    
+            if (!this.isDeleting && this.txt === fullTxt) {
+            delta = this.period;
+            this.isDeleting = true;
+            } else if (this.isDeleting && this.txt === '') {
+            this.isDeleting = false;
+            this.loopNum++;
+            delta = 500;
+            }
+    
+            setTimeout(function() {
+            that.tick();
+            }, delta);
+        };
+    
+        window.onload = function() {
+            var elements = document.getElementsByClassName('typewrite');
+            for (var i=0; i<elements.length; i++) {
+                var toRotate = elements[i].getAttribute('data-type');
+                var period = elements[i].getAttribute('data-period');
+                if (toRotate) {
+                  new TxtType(elements[i], JSON.parse(toRotate), period);
+                }
+            }
+
+        };
